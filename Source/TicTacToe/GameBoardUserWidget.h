cafded51fa47314bd5323a917a671f1e669efa94
Taisen Colcher <TaisenColcher@Gmail.com>
2023-04-19 18:30:05 -0400

    Add GameBoardUserWidget class
    Created UUserWidget GameBoard class.
    Created BP based on class.

---
+++ Source/TicTacToe/GameBoardUserWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GameBoardUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TICTACTOE_API UGameBoardUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
