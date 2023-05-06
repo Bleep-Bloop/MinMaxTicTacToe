22dd625839ee0c380e885150c91d960a1402da45
Taisen Colcher <TaisenColcher@Gmail.com>
2023-05-06 04:08:30 -0400

    Set GameMode's BoardSpace
    Forgot to include this with previous commit. Handles the logic for passing all the buttons from the BoardUserWidget to the GameMode.

--- Source/TicTacToe/GameBoardUserWidget.cpp
+++ Source/TicTacToe/GameBoardUserWidget.cpp
@@ -3,3 +3,31 @@
 
 #include "GameBoardUserWidget.h"
 
+#include "Kismet/GameplayStatics.h"
+
+void UGameBoardUserWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	CurrentGameMode = Cast<ATicTacToeGameModeBase>(UGameplayStatics::GetGameMode(GetWorld()));
+
+	InitButtons();
+	
+}
+
+void UGameBoardUserWidget::InitButtons()
+{
+	// Set References in GameMode
+	CurrentGameMode->BoardSpaces[0][0] = GameButton0_0;
+	CurrentGameMode->BoardSpaces[0][1] = GameButton0_1;
+	CurrentGameMode->BoardSpaces[0][2] = GameButton0_2;
+
+	CurrentGameMode->BoardSpaces[1][0] = GameButton1_0;
+	CurrentGameMode->BoardSpaces[1][1] = GameButton1_1;
+	CurrentGameMode->BoardSpaces[1][2] = GameButton1_2;
+
+	CurrentGameMode->BoardSpaces[2][0] = GameButton2_0;
+	CurrentGameMode->BoardSpaces[2][1] = GameButton2_1;
+	CurrentGameMode->BoardSpaces[2][2] = GameButton2_2;
+
+}
--- Source/TicTacToe/GameBoardUserWidget.h
+++ Source/TicTacToe/GameBoardUserWidget.h
@@ -3,8 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameButton.h"
 #include "Blueprint/UserWidget.h"
 #include "GameBoardUserWidget.generated.h"
+
+class UImage;
 
 /**
  * 
@@ -13,5 +16,47 @@ UCLASS()
 class TICTACTOE_API UGameBoardUserWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+protected:
+	virtual void NativeConstruct() override;
+
+private:
+
+	UPROPERTY()
+	ATicTacToeGameModeBase* CurrentGameMode;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	UPanelWidget* RootWidget;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	UImage* BackgroundImage; // Just a color
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	UImage* GameBoardImage;
+
+	// BoardSpaces // Row_Column 
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	UGameButton* GameButton0_0;
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	UGameButton* GameButton0_1;
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	UGameButton* GameButton0_2;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	UGameButton* GameButton1_0;
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	UGameButton* GameButton1_1;
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	UGameButton* GameButton1_2;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	UGameButton* GameButton2_0;
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	UGameButton* GameButton2_1;
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	UGameButton* GameButton2_2;
+
+	void InitButtons();
+	
 	
 };
