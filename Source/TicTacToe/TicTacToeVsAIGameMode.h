0a5b053ab82de9a893c9da3a0a2c9d834b8a2a9a
Taisen Colcher <TaisenColcher@Gmail.com>
2023-05-06 22:17:20 -0400

    Create Vs Ai Game Mode
    Created a child of GameModeBase to handle playing against an AI.

---
+++ Source/TicTacToe/TicTacToeVsAIGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TicTacToeGameModeBase.h"
+#include "TicTacToeVsAIGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TICTACTOE_API ATicTacToeVsAIGameMode : public ATicTacToeGameModeBase
+{
+	GENERATED_BODY()
+	
+};
